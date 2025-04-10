0ae9d60017c91d8931f563a17c45f3f8fc73660d
Fel_Saya-ao <146634912+SatouriKanade@users.noreply.github.com>
2025-04-10 04:33:42 +0000

    Adding PollOption

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PollOption from '/PollOption';
+import PollOption from './PollOption';
 
 function App() {
   const [count, setCount] = useState(0)
